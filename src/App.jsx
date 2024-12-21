4d15823a5a5b132f1bf41244a9e5cdf6c19fe08c
Smil <149633447+IsaiasSorriso@users.noreply.github.com>
2024-12-21 18:06:35 -0300

    tranferindo o portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from './components/Header'
+import Home from './components/Home'
+import Projects from './components/Projects'
+import About from './components/About'
+import translations from './tradução/tradulation.js';
+import Footer from './components/Footer.jsx';
+import React,{ useState } from "react";
+import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [language, setLanguage] = useState("pt");
+
+  const onLanguageChange = () => {
+      setLanguage(language === "pt" ? "en" : "pt");
+    };
+
+    const texts = translations[language];
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Header onLanguageChange={onLanguageChange}/>,
+    <Home texts={texts} />
+    <Projects texts={texts} />
+    <About texts={texts} />
+    <Footer />
     </>
   )
 }
